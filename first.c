03089f237bb0599967974211a9a1c03edb3383d5
sakethreddypingili <pingilisakethreddy@gmail.com>
2025-11-06 11:40:01 +0530

    initial commit

---
+++ first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    printf("%s", argv);
+}
+// int main(int argc, char *argv[]) {
+// if (argc=n) {
+//     printf(“Company Name %s\n, City:%s\n, Country: %s \n”, agrv[n-2], agrv[n-1], agrv[n]);
+// }
+
+
+
+//     if (argc < 4) { 
+//         printf("Usage: %s <Company Name> <City> <Country>\n", argv[0]);
+//     }
+// printf("Company Name: ");
+//     for (int i = 1; i < argc - 2; i++) {
+//         printf("%s", argv[i]);
+//         if (i < argc - 3) printf(" "); 
+//     }
+// printf("\nCity: %s\nCountry: %s\n", argv[argc - 2], argv[argc - 1]);
+// return 0;
+// }
